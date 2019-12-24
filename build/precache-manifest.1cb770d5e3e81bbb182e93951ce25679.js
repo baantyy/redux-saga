1007350fb4cfdeac943c0c9c9d8dca8d3e6a1f8d
banty-msys <bmaharana@msystechnologies.com>
2019-12-24 15:17:15 +0530

    build created

---
+++ build/precache-manifest.1cb770d5e3e81bbb182e93951ce25679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7ad9c188a04d928a820b414ea9bb1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "850415a8198ef9844d3c",
+    "url": "/static/css/main.d5f3f42a.chunk.css"
+  },
+  {
+    "revision": "d1e6c4e6ecddf172c7d4",
+    "url": "/static/js/2.9bd054db.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.9bd054db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "850415a8198ef9844d3c",
+    "url": "/static/js/main.bd0e8506.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
